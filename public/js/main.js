10494041375a66d637d606c28c09b2458a8c808e
charleandoer <charhuang17@gmail.com>
2018-01-23 22:59:31 -0800

    First Functional
    Website is functional and has OCF requirements

--- public/js/main.js
+++ public/js/main.js
@@ -17,19 +17,57 @@ jQuery(document).ready(function($) {
     });
    
     /* ======= ScrollTo ======= */
-    $('a.scrollto').on('click', function(e){
+ //    $('a.scrollto').on('click', function(e){
         
-        //store hash
-        var target = this.hash;
+ //        //store hash
+ //        var target = this.hash;
                 
-        e.preventDefault();
+ //        e.preventDefault();
         
-		$('body').scrollTo(target, 800, {offset: -70, 'axis':'y', easing:'easeOutQuad'});
-        //Collapse mobile menu after clicking
-		if ($('.navbar-collapse').hasClass('in')){
-			$('.navbar-collapse').removeClass('in').addClass('collapse');
-		}
+	// 	$('body').scrollTo(target, 800, {offset: -70, 'axis':'y', easing:'easeOutQuad'});
+ //        //Collapse mobile menu after clicking
+	// 	if ($('.navbar-collapse').hasClass('in')){
+	// 		$('.navbar-collapse').removeClass('in').addClass('collapse');
+	// 	}
 		
-	});
+	// });
+
+
+    // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
 
 });
